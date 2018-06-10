13e68edccf17e640688a98ca1424be1c82e266b0
Ignacio Álvarez Arenas <ignacio_aaa@hotmail.com>
2018-06-10 01:02:14 -0400

    add: Se describe funcionalidad en Readme y se incluye Dockerfile

---
+++ config.py
@@ -0,0 +1,18 @@
+# Telegram bot Token
+token = "Aquí debes ingresar el Token entregado por el @BotFather de Telegram"
+
+# python -c "import os; print(repr(os.urandom(32)));"
+salt = "Aquí debes incluir una clave secreta. Se sugiere el resultado de la ejecución de la línea 4"
+
+
+#**** No modificar los siguientes campos *****#
+#* Network *#
+
+## Letra c, al inicio de todas las direcciones Chaucha
+magic = 88
+
+## Pago a minero
+fee = 0.001
+
+## Cantidad de ceros decimales o llamados satoshis
+satoshi = 100000000
